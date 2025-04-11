0f1906b152781ea7fc5cbdfcbdf335be93ace811
Antoni Gorski <agorski@c1r8s4.42warsaw.pl>
2025-04-11 17:12:21 +0200

    geter and seter to contact wrote

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -6,33 +6,63 @@
 /*   By: agorski <agorski@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 15:45:31 by agorski           #+#    #+#             */
-/*   Updated: 2025/04/10 15:12:21 by agorski          ###   ########.fr       */
+/*   Updated: 2025/04/11 17:11:28 by agorski          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
 
-Contact::Contact()
-{
-    FirstName = "";
-    LastName = "";
-    Nickname = "";
-    PhoneNumber = "";
-    DarkestSecret = "";
-}
-
-Contact::Contact( std::string FirstName,  std::string LastName,  std::string Nickname,  std::string PhoneNumber,  std::string DarkestSecret)
+void Contact::setFirstName(std::string FirstName)
 {
     this->FirstName = FirstName;
+}
+
+void Contact::setLastName(std::string LastNAme)
+{
     this->LastName = LastName;
+}
+
+void Contact::setNickname(std::string Nickname)
+{
     this->Nickname = Nickname;
+}
+
+void Contact::setPhoneNumber(std::string PhoneNumber)
+{
     this->PhoneNumber = PhoneNumber;
+}
+
+void Contact::setDarknestSecret(std::string DarkestSecret)
+{
     this->DarkestSecret = DarkestSecret;
 }
 
-std::string Contact::getContact()
+std::string Contact::getFirstName()
 {
+    return this->FirstName;
+}
 
-        std::cout << std::setw(10) << 
+std::string Contact::getLastName()
+{
+    return this->LastName;
+}
+
+std::string Contact::getNickname()
+{
+    return this->Nickname;
+}
+
+std::string Contact::getPhoneNumber()
+{
+    return this->PhoneNumber;
+}
+
+std::string Contact::getDarkestSecret()
+{
+    return this->DarkestSecret;
+}
+
+void Contact::showContactDetails(int index)
+{
     
 }
--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,11 +6,9 @@
 /*   By: agorski <agorski@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 15:44:13 by agorski           #+#    #+#             */
-/*   Updated: 2025/04/10 15:11:55 by agorski          ###   ########.fr       */
+/*   Updated: 2025/04/11 17:06:34 by agorski          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-
 
 #ifndef CONTACT_HPP
 # define CONTACT_HPP
@@ -29,9 +27,20 @@ private:
     std::string DarkestSecret;
     
 public:
-    Contact();
-    Contact( std::string FirstName,  std::string LastName,  std::string Nickname,  std::string PhoneNumber,  std::string DarkestSecret);
-    std::string getContact();
+    //seters
+    void setFirstName(std::string FirstName);
+    void setLastName(std::string LastName);
+    void setNickname(std::string Nickname);
+    void setPhoneNumber(std::string PhoneNumber);
+    void setDarknestSecret(std::string DarkestSecret);
+    //geters
+    std::string getFirstName();
+    std::string getLastName();
+    std::string getNickname();
+    std::string getPhoneNumber();
+    std::string getDarkestSecret();
+    //other methods
+    void showContactDetails(int index);
 };
 
 #endif
